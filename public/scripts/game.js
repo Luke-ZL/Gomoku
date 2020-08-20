ab93da9490f7fd82ed51997452b52e3135eb6b34
Zixuan Lu <luzixuanaa@gmail.com>
2020-08-19 22:54:30 -0700

    BUG fix and add debug mod
    debug mode is enabled when set DEBUG = 1

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,23 +1,55 @@
-const BLACK = 0, WHITE = 1;
+const BLACK = 1, WHITE = -1, DEBUG = 1;
 var playerColor = BLACK; //default is black
+var board = [ 
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+  ]
+
+if (DEBUG) {
+    $("td").addClass("debug");
+}
 
 $("table").on("click", "td", function(){
-    console.log("%s", $(this).attr("id"));
+    let indexArr = $(this).attr("id").split("i");
+    let row = parseInt(indexArr[0], 10), col = parseInt(indexArr[1], 10);
+
+    //TODO: check move validity
+    board[row][col] = playerColor;
+    console.log(board);
     add(playerColor, $(this).attr("id"));
 });
 
 $(".resetBtn").on("click", function() {
-    console.log($(this).attr("id"));
-    playerColor = $(this).attr("id") === "buttonBlack" ? 0 : 1;
+    playerColor = $(this).attr("id") === "buttonBlack" ? BLACK : WHITE;
     reset();
 });
 
 function reset() {
-    console.log(playerColor);
+    $("td").removeClass("blackPawn whitePawn");
+    for(let i in board) {
+        for(let j in board) {
+            board[i][j] = 0;
+        }
+    }
 }
 
 function add(color, id) {
-    if (color == BLACK) {
+    if (color === BLACK) {
        $("#" + id).addClass("blackPawn");  // certainid which includes ' ', '.' etc. needs to be escaped, so I use 'i' as delim
+    } else {
+        $("#" + id).addClass("whitePawn");
     }
 }
